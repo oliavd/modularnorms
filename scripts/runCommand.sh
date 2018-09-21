46e19215829337a0ffa8d4f80b546ed80422225b
Olivier Avande <mavande@unomaha.edu>
2018-09-21 13:26:49 -0500

    updated apache2_full.owl

--- scripts/runCommand.sh
+++ scripts/runCommand.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python gennomos.py ../examples/newExamples/apache2_oli.json dot > ../examples/newExamples/apache2_oli.dot && python gennomos.py ../examples/newExamples/apache2_oli.json owl > ../examples/newExamples/apache2_oli.owl &&  dot -Tsvg -o ../examples/newExamples/apache2_oli.svg ../examples/newExamples/apache2_oli.dot && ./xformsvg ../examples/newExamples/apache2_oli > ../examples/newExamples/apache2_oli.html
+python gennomos.py ../examples/newExamples/apache2_full.json dot > ../examples/newExamples/apache2_full.dot && python gennomos.py ../examples/newExamples/apache2_full.json owl > ../examples/newExamples/apache2_full.owl &&  dot -Tsvg -o ../examples/newExamples/apache2_full.svg ../examples/newExamples/apache2_full.dot && ./xformsvg ../examples/newExamples/apache2_full > ../examples/newExamples/apache2_full.html
 
 
